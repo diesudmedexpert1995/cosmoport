5acfdc789de3070b371b70a2d5d147cf3109be1c
diesudmedexpert1995 <diesudmedexpert1995@gmail.com>
2021-01-23 16:27:20 +0200

    Complete first stage of App

--- src/main/java/com/space/utils/ShipSpecificationUtil.java
+++ src/main/java/com/space/utils/ShipSpecificationUtil.java
@@ -8,87 +8,92 @@ import java.sql.Date;
 
 public class ShipSpecificationUtil {
 
-    public static Specification<Ship> getShipSpecification(String name, String planet, ShipType shipType, Long before, Long after, Boolean isUsed,  Double minSpeed, Double maxSpeed, Integer minCrewSize, Integer maxCrewSize, Double minRating, Double maxRating){
-        return Specification.where(
-                ShipSpecificationUtil.calculateShipsByName(name)
-                .and(ShipSpecificationUtil.calculateShipsByPlanet(planet))
-                .and(ShipSpecificationUtil.calculateShipsByDate(before, after))
-                .and(ShipSpecificationUtil.calculateShipsByShipType(shipType))
-                .and(ShipSpecificationUtil.calculateShipsByUsage(isUsed))
-                .and(ShipSpecificationUtil.calculateShipsBySpeed(minSpeed, maxSpeed))
-                .and(ShipSpecificationUtil.calculateShipsByCrewSize(minCrewSize,maxCrewSize))
-                .and(ShipSpecificationUtil.calculateShipsByRating(minRating, maxRating))
-        );
+    public static Specification<Ship> getShipSpecification(String name,
+                                                           String planet,
+                                                           ShipType shipType,
+                                                           Long after, Long before,
+                                                           Boolean isUsed,
+                                                           Double minSpeed, Double maxSpeed,
+                                                           Integer minCrewSize, Integer maxCrewSize,
+                                                           Double minRating, Double maxRating) {
+        return Specification.where(ShipSpecificationUtil.shipsByName(name)
+                .and(ShipSpecificationUtil.shipsByPlanet(planet)))
+                .and(ShipSpecificationUtil.shipsByShipType(shipType))
+                .and(ShipSpecificationUtil.shipsByDate(after, before))
+                .and(ShipSpecificationUtil.shipsByUsage(isUsed))
+                .and(ShipSpecificationUtil.shipsBySpeed(minSpeed, maxSpeed))
+                .and(ShipSpecificationUtil.shipsByCrewSize(minCrewSize, maxCrewSize))
+                .and(ShipSpecificationUtil.shipsByRating(minRating, maxRating));
     }
 
-    public static Specification<Ship> calculateShipsByRating(Double min, Double max){
-        return (root, criteriaQuery, criteriaBuilder) -> {
-            if(min == null &&  max == null) return null;
-            if(min == null) return criteriaBuilder.lessThanOrEqualTo(root.get("rating"), max);
-            if(max == null) return criteriaBuilder.greaterThanOrEqualTo(root.get("rating"), min);
-            return criteriaBuilder.between(root.get("rating"),min,max);
+    public static Specification<Ship> shipsByRating(Double min, Double max) {
+        return (r, q, cb) -> {
+            if (min == null && max == null) return null;
+            if (min == null)                return cb.lessThanOrEqualTo(r.get("rating"), max);
+            if (max == null)                return cb.greaterThanOrEqualTo(r.get("rating"), min);
+            return cb.between(r.get("rating"), min, max);
         };
     }
 
-    public static Specification<Ship> calculateShipsByName(String name){
-        return (root, criteriaQuery, criteriaBuilder) ->  name == null ? null: criteriaBuilder.like(root.get("name"), "%" + name + "%");
-
+    public static Specification<Ship> shipsByName(String name) {
+        return (r, q, cb) -> name == null ? null : cb.like(r.get("name"), "%" + name + "%");
     }
 
-    public static Specification<Ship> calculateShipsByPlanet(String planet){
-        return (root, criteriaQuery, criteriaBuilder) -> planet == null ? null : criteriaBuilder.like(root.get("planet"), "%" + planet + "%");
 
+    public static Specification<Ship> shipsByPlanet(String planet) {
+        return (r, q, cb) -> planet == null ? null : cb.like(r.get("planet"), "%" + planet + "%");
     }
 
-    public static Specification<Ship> calculateShipsByShipType(ShipType shipType){
-        return (root, criteriaQuery, criteriaBuilder) -> shipType == null ? null: criteriaBuilder.equal(root.get("shipType"), shipType);
+
+    public static Specification<Ship> shipsByShipType(ShipType shipType) {
+        return (r, q, cb) -> shipType == null ? null : cb.equal(r.get("shipType"), shipType);
     }
 
-    public static Specification<Ship> calculateShipsByDate(Long before, Long after){
-        return (root, criteriaQuery, criteriaBuilder) -> {
+
+    public static Specification<Ship> shipsByDate(Long after, Long before) {
+        return (r, q, cb) -> {
             if (after == null && before == null) return null;
             if (after == null) {
-               Date dateBefore = new Date(before);
-               return criteriaBuilder.lessThanOrEqualTo(root.get("prodDate"), dateBefore);
+                Date before1 = new Date(before);
+                return cb.lessThanOrEqualTo(r.get("prodDate"), before1);
             }
-
             if (before == null) {
-                Date dateAfter = new Date(after);
-                return criteriaBuilder.greaterThanOrEqualTo(root.get("prodDate"), dateAfter);
+                Date after1 = new Date(after);
+                return cb.greaterThanOrEqualTo(r.get("prodDate"), after1);
             }
-
-            Date dateBefore = new Date(before);
-            Date dateAfter = new Date(after);
-            return criteriaBuilder.between(root.get("prodDate"), dateBefore, dateAfter);
+            Date before1 = new Date(before);
+            Date after1 = new Date(after);
+            return cb.between(r.get("prodDate"), after1, before1);
         };
     }
 
-    public static Specification<Ship> calculateShipsByUsage(Boolean isUsed){
-        return (root, criteriaQuery, criteriaBuilder) -> {
+
+    public static Specification<Ship> shipsByUsage(Boolean isUsed) {
+        return (r, q, cb) -> {
             if (isUsed == null) return null;
-            if (isUsed) return criteriaBuilder.isTrue(root.get("isUsed"));
-            return criteriaBuilder.isFalse(root.get("isUsed"));
+            if (isUsed)         return cb.isTrue(r.get("isUsed"));
+            else return cb.isFalse(r.get("isUsed"));
         };
-
     }
 
-    public static Specification<Ship> calculateShipsBySpeed(Double min, Double max){
-        return (root, criteriaQuery, criteriaBuilder) -> {
+
+    public static Specification<Ship> shipsBySpeed(Double min, Double max) {
+        return (r, q, cb) -> {
             if (min == null && max == null) return null;
-            if (min == null)                return criteriaBuilder.lessThanOrEqualTo(root.get("speed"), max);
-            if (max == null)                return criteriaBuilder.greaterThanOrEqualTo(root.get("speed"), min);
-            return criteriaBuilder.between(root.get("speed"), min, max);
+            if (min == null)                return cb.lessThanOrEqualTo(r.get("speed"), max);
+            if (max == null)                return cb.greaterThanOrEqualTo(r.get("speed"), min);
+            return cb.between(r.get("speed"), min, max);
         };
     }
 
-    public static Specification<Ship> calculateShipsByCrewSize(Integer min, Integer max){
-        return (root, criteriaQuery, criteriaBuilder)-> {
+
+    public static Specification<Ship> shipsByCrewSize(Integer min, Integer max) {
+        return (r, q, cb) -> {
             if (min == null && max == null) return null;
-            if (min == null)                return criteriaBuilder.lessThanOrEqualTo(root.get("crewSize"), max);
-            if (max == null)                return criteriaBuilder.greaterThanOrEqualTo(root.get("crewSize"), min);
-            return criteriaBuilder.between(root.get("crewSize"), min, max);
+            if (min == null)                return cb.lessThanOrEqualTo(r.get("crewSize"), max);
+            if (max == null)                return cb.greaterThanOrEqualTo(r.get("crewSize"), min);
+            return cb.between(r.get("crewSize"), min, max);
         };
     }
-
 
 }
